acbe1f8e2bf2423852b0635ae43ada68159bceec
root <root@stg-devops.asia-east1-b.c.nownews-news-stg.internal>
2024-07-26 18:33:20 +0800

    gitlab ci opt

--- shell/push_to_cloudrun.sh
+++ shell/push_to_cloudrun.sh
@@ -8,21 +8,23 @@
 gcloud config set project $GCP_PROJECT_NAME
 # gcloud auth configure-docker asia-docker.pkg.dev
 
-gcloud artifacts repositories create $AR_PROJECT_NAME --repository-format=docker --location=asia --description="Docker repository"
-docker build -t asia-docker.pkg.dev/$GCP_PROJECT_NAME/$AR_PROJECT_NAME/$PROJECT_NAME:$APP_VERSION .
-docker push asia-docker.pkg.dev/$GCP_PROJECT_NAME/$AR_PROJECT_NAME/$PROJECT_NAME:$APP_VERSION
+gcloud auth list
+gcloud compute instances list
+# gcloud artifacts repositories create $AR_PROJECT_NAME --repository-format=docker --location=asia --description="Docker repository"
+# docker build -t asia-docker.pkg.dev/$GCP_PROJECT_NAME/$AR_PROJECT_NAME/$PROJECT_NAME:$APP_VERSION .
+# docker push asia-docker.pkg.dev/$GCP_PROJECT_NAME/$AR_PROJECT_NAME/$PROJECT_NAME:$APP_VERSION
 
-gcloud run deploy $CLOUDRUN_SERVICE \
-    --image=asia-docker.pkg.dev/$GCP_PROJECT_NAME/$AR_PROJECT_NAME/$PROJECT_NAME:$APP_VERSION \
-    --region=asia-east1 \
-    --platform=managed \
-    --allow-unauthenticated \
-    --memory=512Mi \
-    --cpu=1 \
-    --max-instances=3 \
-    --timeout=10m \
-    --concurrency=1 \
-    --set-env-vars=db_user=dev,db_password=00000000
+# gcloud run deploy $CLOUDRUN_SERVICE \
+#     --image=asia-docker.pkg.dev/$GCP_PROJECT_NAME/$AR_PROJECT_NAME/$PROJECT_NAME:$APP_VERSION \
+#     --region=asia-east1 \
+#     --platform=managed \
+#     --allow-unauthenticated \
+#     --memory=512Mi \
+#     --cpu=1 \
+#     --max-instances=3 \
+#     --timeout=10m \
+#     --concurrency=1 \
+#     --set-env-vars=db_user=dev,db_password=00000000
 
 
 # gcloud run services delete my-service5 --platform=managed --region=asia-east1
